1515831317a9a8bc160d6703773b69837f4422bc
Mohammad-haydar <50136482+Mohammad-haydar@users.noreply.github.com>
2019-05-13 23:36:45 +0200

    restaurant

---
+++ js/Javascript.js
@@ -0,0 +1,15 @@
+
+$('#food, #crew,#navbar a,#Heading').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100
+      },
+      900
+    );
+  }
+});
